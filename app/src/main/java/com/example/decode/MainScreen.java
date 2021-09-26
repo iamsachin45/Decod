eafa9bb306a4cf0c735c186401c4470824103dee
indianhacker4 <56166702+indianhacker4@users.noreply.github.com>
2021-09-26 22:07:21 +0530

    Connect with firebase

---
+++ app/src/main/java/com/example/decode/MainScreen.java
@@ -0,0 +1,16 @@
+package com.example.decode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
